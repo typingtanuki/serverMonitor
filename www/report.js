8b06fd4ac7060b1a22910d43b05cc2a4a7f97047
Mathias Clerc <typingtanuki@gmail.com>
2020-03-03 22:21:38 +0900

    * Split report method to own file * Improve updates display

--- www/report.js
+++ www/report.js
@@ -1,3 +1,4 @@
+
 function formatReports(reports, node) {
     while (node.hasChildNodes()) {
         node.removeChild(node.firstChild);
@@ -25,6 +26,26 @@ function formatReportDetails(report) {
     const details = report.details;
     const keys = Object.keys(details);
 
+    const hidden = [];
+
+    for (let i = 0; i < keys.length; i++) {
+        const entry = details[keys[i]];
+        if (isHistory(entry)) {
+            const sub = document.createElement("li");
+            element.appendChild(sub);
+            const values = entry["values"];
+            const dates = entry["dates"];
+            const max = parseInt(entry["max"]);
+            const limit = parseInt(entry["limit"]);
+            sub.appendChild(makeLine(values, dates, max, limit));
+            hidden.push(keys[i]);
+        }
+        if (keys[i] === "Updates") {
+            makeList(entry, hidden, element);
+            hidden.push(keys[i]);
+        }
+    }
+
     if (keys.indexOf("Current Usage") !== -1 && keys.indexOf("Maximum Usage") !== -1) {
         const sub = document.createElement("li");
         element.appendChild(sub);
@@ -33,10 +54,18 @@ function formatReportDetails(report) {
         sub.appendChild(makeGauge(current, max));
     }
 
+    makeList(details, hidden, element);
+    return element;
+}
+
+function makeList(details, hidden, element) {
+    const keys = Object.keys(details);
     for (let i = 0; i < keys.length; i++) {
+        if (hidden.indexOf(keys[i]) > -1) {
+            continue;
+        }
         const sub = document.createElement("li");
         element.appendChild(sub);
-
         const detail = document.createElement("span");
         const key = document.createElement("span");
         key.classList.add("key");
@@ -48,5 +77,8 @@ function formatReportDetails(report) {
         detail.appendChild(value);
         sub.appendChild(detail);
     }
-    return element;
+}
+
+function isHistory(value) {
+    return value.hasOwnProperty("type") && value["type"] === "history";
 }
